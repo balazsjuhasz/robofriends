d09bdb25a246dc05147b4f0acc082f4273546f0a
Balazs Juhasz <juhaszbal@gmail.com>
2020-06-21 15:15:59 +0200

    add error bounds

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,20 +2,16 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error("NOOOOOO");
+  }
   return (
     <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={user.id}
-              name={user.name}
-              email={user.email}
-            />
-          );
-        })
-      }
+      {robots.map((user, i) => {
+        return (
+          <Card key={i} id={user.id} name={user.name} email={user.email} />
+        );
+      })}
     </div>
   );
 };
